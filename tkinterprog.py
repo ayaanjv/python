5166f57c45c6dec6f9284c72ebf9a78523b0db45
bluecoder33 <45527807+bluecoder33@users.noreply.github.com>
2020-02-18 11:45:57 -0800

    Update tkinterprog.py

--- tkinterprog.py
+++ tkinterprog.py
@@ -11,5 +11,20 @@ e1=Entry(master)
 e1.grid(row=0,column=1)
 e1.insert(END,"Enter Your Name Here")
 e1.delete(0,END)
+e=e1.get()
+    e=int(e)
+    e=e*9/5+32
+    l3=Label(master, text=e).grid(row=0,column=3)
+
+    
+    
+l1=Label(master, text="Celsius").grid(row=0)
+l2=Label(master, text="Fahrenheit").grid(row=0,column=2)
+b1=Button(master, text='Convert',command=print_text).grid(row=1,column =0)
+e1=Entry(master)
+e1.grid(row=0,column=1)
+e1.insert(END,"Enter Your Name Here")
+e1.delete(0,END)
+
 
 mainloop()
